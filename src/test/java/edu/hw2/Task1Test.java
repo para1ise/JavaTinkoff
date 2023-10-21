17a9e6988231383d70b230dd836c7e97fff66727
Айдыс Иргит <aydysirgit@gmail.com>
2023-10-21 22:47:29 +0700

    hw2: edits in Task1Test

--- src/test/java/edu/hw2/Task1Test.java
+++ src/test/java/edu/hw2/Task1Test.java
@@ -2,6 +2,7 @@ package edu.hw2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task1Test {
@@ -16,8 +17,8 @@ public class Task1Test {
         var exp = new Task1.Expr.Exponent(mul, 2);
         var res = new Task1.Expr.Addition(exp, new Task1.Expr.Constant(1));
 
-        System.out.println(res + " = " + res.evaluate());
-        assertThat(res).isEqualTo(1);
+        assertThat(new double[] {negOne.evaluate(), sumTwoFour.evaluate(), mul.evaluate(), exp.evaluate(),
+            res.evaluate()}).isEqualTo(new double[] {-1, 6, -6, 36, 37});
     }
 }
 
